b30a99fa32d0795e61def46258ba330db26d4283
NikLeberg <niklaus.leuenb+github@gmail.com>
2021-01-01 21:21:47 +0100

    update for sensors

--- main/main.c
+++ main/main.c
@@ -29,9 +29,9 @@
 void app_main(void) {
     printf("Hallo quadro3!\n");
 
-    printf("starte i2c: %u\n", i2cStart());
-
     printf("starte sensors: %u\n", sensorsStart());
+
+    printf("starte i2c: %u\n", i2cStart());
 
     printf("starte bno: %u\n", bnoStart());
 
@@ -40,16 +40,16 @@ void app_main(void) {
     int64_t lastTimestamp = 0;
     sensorsData_t state = {0};
     while (true) {
-        vTaskDelay(100 / portTICK_PERIOD_MS);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
         lastTimestamp = state.timestamp;
-        sensorsGet(SENSORS_STATE_ORIENTATION, SENSORS_ENU_LOCAL, &state);
+        sensorsGetState(SENSORS_STATE_ORIENTATION, SENSORS_ENU_LOCAL, &state);
         printf("time:\t%llu\t%llu\n", state.timestamp, state.timestamp - lastTimestamp);
         printf("orient:\t%f\t%f\t%f\t%f\n", state.quaternion.real, state.quaternion.i, state.quaternion.j, state.quaternion.k);
-        sensorsGet(SENSORS_STATE_EULER, SENSORS_ENU_WORLD, &state);
+        sensorsGetState(SENSORS_STATE_EULER, SENSORS_ENU_WORLD, &state);
         printf("euler:\t%f\t%f\t%f\n", state.vector.x, state.vector.y, state.vector.z);
-        sensorsGet(SENSORS_STATE_ACCELERATION, SENSORS_ENU_WORLD, &state);
+        sensorsGetState(SENSORS_STATE_ACCELERATION, SENSORS_ENU_WORLD, &state);
         printf("accel:\t%f\t%f\t%f\n", state.vector.x, state.vector.y, state.vector.z);
-        sensorsGet(SENSORS_STATE_ROTATION, SENSORS_ENU_WORLD, &state);
+        sensorsGetState(SENSORS_STATE_ROTATION, SENSORS_ENU_WORLD, &state);
         printf("rotate:\t%f\t%f\t%f\n", state.vector.x, state.vector.y, state.vector.z);
     }
 }
